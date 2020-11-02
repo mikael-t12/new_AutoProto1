a651058866e214452336be65a1f9b2f5e35f79e9
Mikael <cmito004@edu.xamk.fi>
2020-11-02 11:40:38 +0200

    Add everything

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    //Offset variable for the camera position
+    private Vector3 offset = new Vector3(0, 4, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float carSpeed = 24.5f;
+    private float turnSpeed = 35.4f;
+
+    private float forwardInput;
+    private float horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        //Moving the vehicle forward, if the player presses to forward key.
+        transform.Translate(Vector3.forward * Time.deltaTime *carSpeed *forwardInput);
+        //Horizontal input of the vehicle, rotates the vehicle
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+}
